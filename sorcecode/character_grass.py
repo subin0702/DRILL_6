130f8c0491ceed1b1ca84c4e558101a6c26096ef
subin0702 <tbasl0702@naver.com>
2021-09-16 15:02:32 +0900

    움직이기 실습 추가

--- sorcecode/character_grass.py
+++ sorcecode/character_grass.py
@@ -2,6 +2,19 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 0
+while x < 800:
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,90)
+    x += 2
+    delay(0.01)
+
+    
+
+delay(5)
 
 close_canvas()
